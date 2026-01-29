0db969b6b80c690ee9361c647f1207946cc4e48d
CreeperC418 <xx416168ye@outlook.com>
2026-01-30 00:32:30 +0800

    Add main.c for P1009

--- C/acm/luogu/P/1009/main.c
+++ C/acm/luogu/P/1009/main.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
-#include <math.h>
+#include <string.h>
 
-int main(){
-    unsigned long long n;
-    unsigned long long ans = 0;
-    unsigned long long ftl = 1;
-    
-    scanf("%d", &n);
-    if (n <= 25)
-    {    
-        for (unsigned long long i = 1; i <= n; i++)
+void multiply(char *a,char *b) {
+    int a1[100];
+    int b1[100];
+    int len = strlen(a) + strlen(b);
+    int result[200] = {0};
+    for (int i = 0; i < strlen(a); i++)
+        a1[i] = a[strlen(a) - 1 - i] - '0';
+    for (int i = 0; i < strlen(b); i++)
+        b1[i] = b[strlen(b) - 1 - i] - '0';
+    for (size_t i = 0; i < strlen(b); i++)
+    {
+        for (size_t j = 0; j < strlen(a); j++)
         {
-            ftl *= i;
-            ans += ftl;
-        }   
-        printf("%llu\n", ans);
-    }else{
-        ans = 16158688122606784831230313;
-        ftl = 15511210043330985984000000;
-        for (unsigned long long i = 26; i <= n; i++)
-        {
-            ftl *= i;   
-            ans += ftl;
-        }   
+            result[i + j] += a1[j] * b1[i];
+            if (result[i + j] >= 10) {
+                result[i + j + 1] += result[i + j] / 10;
+                result[i + j] = result[i + j] % 10;
+            }
+        }
     }
-        return 0;
+     while (result[len] == 0 && len > 1) len--;
+    for (int i = 0; i <= len; i++)
+        a[i] = result[len - i] + '0';
+}
+int main() {
+    char cnt[100] = "1";
+    char result[200] = "1";
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i <= n; i++) {
+        multiply(result, cnt);
+        cnt[0]++;
+        for (size_t i = 0; i < strlen(cnt); i++)
+        {
+            if (cnt[i] > '9') {
+                cnt[i] = '0';
+                cnt[i + 1]++;
+            }
+        }
+        
+    }   
+    printf("%s\n", result);
     
+    return 0;
 }
