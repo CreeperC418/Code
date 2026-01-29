0c957173a5155b6074c26b621bfd21befaf0c76f
CreeperC418 <xx416168ye@outlook.com>
2026-01-30 03:07:03 +0800

    完成了洛谷p1009

--- C/acm/luogu/P/1009/main.c
+++ C/acm/luogu/P/1009/main.c
@@ -1,48 +1,66 @@
 #include <stdio.h>
 #include <string.h>
 
-void multiply(char *a,char *b) {
-    int a1[100];
-    int b1[100];
-    int len = strlen(a) + strlen(b);
-    int result[200] = {0};
-    for (int i = 0; i < strlen(a); i++)
-        a1[i] = a[strlen(a) - 1 - i] - '0';
-    for (int i = 0; i < strlen(b); i++)
-        b1[i] = b[strlen(b) - 1 - i] - '0';
-    for (size_t i = 0; i < strlen(b); i++)
-    {
-        for (size_t j = 0; j < strlen(a); j++)
-        {
-            result[i + j] += a1[j] * b1[i];
-            if (result[i + j] >= 10) {
-                result[i + j + 1] += result[i + j] / 10;
-                result[i + j] = result[i + j] % 10;
-            }
-        }
-    }
-     while (result[len] == 0 && len > 1) len--;
-    for (int i = 0; i <= len; i++)
-        a[i] = result[len - i] + '0';
-}
 int main() {
-    char cnt[100] = "1";
-    char result[200] = "1";
+    int ans[100] = {0};
+    int a[100] = {0};
+    int b[100] = {0};
+    int result[100] = {0};
+    a[0] = 1;
+    int len_a = 1;
+    int len_b = 0;
+    int len_result = 1;
+    int len_ans = 0;
     int n;
     scanf("%d", &n);
-    for (int i = 0; i <= n; i++) {
-        multiply(result, cnt);
-        cnt[0]++;
-        for (size_t i = 0; i < strlen(cnt); i++)
+    for (int j = 1; j <= n; j++)
+    {
+        for (int i = 1; i <= j; i++) {
+            int p=i;
+            while(p>0){
+                b[len_b++]=p%10;
+                p/=10;
+            }
+            for (size_t i = 0; i < len_b; i++)
+            {
+                for (size_t j = 0; j < len_a; j++)
+                {
+                    result[i + j] += a[j] * b[i];
+                    if (result[i + j] >= 10) {
+                        result[i + j + 1] += result[i + j] / 10;
+                        result[i + j] = result[i + j] % 10;
+                    }
+                }
+            }
+            len_result = len_a + len_b + 1;
+            while(result[len_result-1]==0&&len_result>1){
+                len_result--;
+            }
+            for (int i = 0; i < len_result; i++)
+            {
+                a[i] = result[i];
+                result[i] = 0;
+            }
+            len_a = len_result;
+            len_b = 0;
+        }
+        memset(b, 0, sizeof(b) );
+        memset(result, 0, sizeof(result) );
+        len_ans = len_a > len_ans ? len_a : len_ans;
+        for (size_t i = 0; i < len_a; i++)
         {
-            if (cnt[i] > '9') {
-                cnt[i] = '0';
-                cnt[i + 1]++;
+            ans[i] += a[i];
+            if (ans[i] >= 10) {
+                ans[i + 1] += ans[i] / 10;
+                ans[i] = ans[i] % 10;
             }
         }
-        
-    }   
-    printf("%s\n", result);
-    
+        memset(a, 0, sizeof(a) );
+        len_a = 1;
+        a[0] = 1;
+    }
+    while(!ans[len_ans-1]&&len_ans>1) len_ans--;
+    for(int i=len_ans-1;i>=0;i--) printf("%d", ans[i]); 
+    printf("\n");
     return 0;
 }
